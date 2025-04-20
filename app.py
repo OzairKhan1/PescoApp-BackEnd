0278a1dfe354a723f1526b1363c3dad1d9f17c4a
Ozair Khan <ozairk048@gmail.com>
2025-04-20 16:15:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py (Flask backend)
 from flask import Flask, request, jsonify
 from selenium_script import get_customer_id
+import os
 
 app = Flask(__name__)
 
@@ -16,4 +17,5 @@ def get_customer():
     return jsonify({"customer_id": customer_id})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
